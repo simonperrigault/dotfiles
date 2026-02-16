a7ae70bc8dc846cfcb658a23bd44c337d245d9ea
Simon Perrigault <simonperrigault22@gmail.com>
2026-02-16 21:43:47 +0100

    replace nvim config by lazyvim

---
+++ .config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,22 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+        "TmuxNavigatorProcessList",
+    },
+    keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+}
